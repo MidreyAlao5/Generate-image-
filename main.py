485310ccb934b34b3260552f98cccc7c7717bdfc
MidreyAlao5 <olamidealao81@hotmail.com>
2025-03-28 19:06:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -57,5 +57,9 @@ async def main():
 
 # Run the bot safely
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    try:
+        asyncio.run(main())
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(main())
